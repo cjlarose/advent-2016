83fcc7b38a50a932dc9caec2c9df3a8a822714a8
Chris LaRose <cjlarose@gmail.com>
2016-12-10 09:43:26 -0800

    Add parser for day 1 input

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,34 @@
 module Main where
 
+import System.IO (readFile)
+import Text.Parsec (parse)
+import Text.Parsec.Char (char, digit, string)
+import Text.Parsec.Prim ((<|>))
+import Text.Parsec.Combinator (many1, sepBy, eof, endBy)
+
+data Direction = L | R deriving Show
+
+rotateR = do
+  res <- char 'R'
+  return R
+
+rotateL = do
+  res <- char 'L'
+  return L
+
+movement = do
+  res <- many1 digit
+  return (read res :: Int)
+
+rotationMovement = do
+  dir <- rotateR <|> rotateL
+  steps <- movement
+  return (dir, steps)
+
+instructionList = (rotationMovement `sepBy` string ", ") `endBy` char '\n'
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  contents <- readFile "inputs/01.txt"
+  let result5 = parse instructionList "" contents
+  print result5
