80b7f5fb071b0cb5b6ce918bb9117b0771d9b851
Chris LaRose <cjlarose@gmail.com>
2016-12-11 09:32:41 -0800

    Complete Day 3, Part 2

--- src/SquaresWithThreeSides.hs
+++ src/SquaresWithThreeSides.hs
@@ -29,11 +29,18 @@ triplets = do
 isTriangle :: (Int, Int, Int) -> Bool
 isTriangle (a, b, c) = a + b > c && a + c > b && b + c > a
 
+transpose :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+transpose [] = []
+transpose ((a, b, c) : (d, e, f) : (g, h, i) : xs) = (a, d, g) : (b, e, h) : (c, f, i) : transpose xs
+
 solve :: String -> IO ()
 solve input = do
   let triangleData = parse triplets "" input
   case triangleData of
     Left err -> print err
     Right ts -> do
-      let numTriangles = length . filter isTriangle $ ts
+      let countTriangles = length . filter isTriangle
+      let numTriangles = countTriangles ts
+      let numTrianglesTransposed = countTriangles . transpose $ ts
       print numTriangles
+      print numTrianglesTransposed
