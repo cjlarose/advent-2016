1b4d6961b494a85d1c83fa50a62db1e26fcb4cc4
Chris LaRose <cjlarose@gmail.com>
2016-12-11 23:00:14 -0800

    Speed up Day 5, part 1 fast enough to complete it

--- src/Chess.hs
+++ src/Chess.hs
@@ -5,24 +5,21 @@ import Data.Char (isSpace)
 import qualified Data.ByteString as B
 import Data.ByteString.Char8 (pack)
 import qualified Crypto.Hash.MD5 as MD5
+import qualified Data.NibbleString as NS
 
 allHashes :: String -> [B.ByteString]
 allHashes prefix = map (MD5.finalize . MD5.update prefixCtx . pack . show) [0..]
   where
     prefixCtx = MD5.update MD5.init $ pack prefix
 
-hasZerosPrefix :: Int -> String -> Bool
-hasZerosPrefix n = all (== '0') . take n
-
-toHex :: B.ByteString -> String
-toHex = concatMap (`showHex` "") . B.unpack
-
 doorCode :: String -> String
 doorCode = take 8 .
-           map (!! 5) .
-           filter (hasZerosPrefix 5) .
-           map toHex .
+           map (head . (`showHex` "") . NS.head . NS.drop 5) .
+           filter (NS.isPrefixOf expectedPrefix) .
+           map NS.EvenNibbleString .
            allHashes
+  where
+    expectedPrefix = NS.pack $ replicate 5 0
 
 solve :: String -> IO ()
 solve input = do
