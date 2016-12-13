eafef0a97cd89a80c62b5ec2c64e080f83730cf6
Chris LaRose <cjlarose@gmail.com>
2016-12-13 00:31:16 -0800

    Complete Day 6, part 1

--- src/SignalsAndNoise.hs
+++ src/SignalsAndNoise.hs
@@ -1,8 +1,16 @@
 module SignalsAndNoise (solve) where
 
 import Data.String (lines)
+import Data.List (group, sort, transpose)
+import Control.Arrow ((&&&))
+
+mostFrequentChar :: String -> Char
+mostFrequentChar = snd . minimum . map ((negate . length) &&& head) . group . sort
+
+signal :: [String] -> String
+signal = map mostFrequentChar . transpose
 
 solve :: String -> IO ()
 solve input = do
   let messages = lines input
-  print messages
+  putStrLn $ signal messages
