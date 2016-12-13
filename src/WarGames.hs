a0fb7e0e1a4eeabfa1e570dd9da347050686a058
Chris LaRose <cjlarose@gmail.com>
2016-12-12 22:19:32 -0800

    Separate out interesting hash predicate

--- src/WarGames.hs
+++ src/WarGames.hs
@@ -7,19 +7,23 @@ import Data.ByteString.Char8 (pack)
 import qualified Crypto.Hash.MD5 as MD5
 import qualified Data.NibbleString as NS
 
-allHashes :: String -> [B.ByteString]
-allHashes prefix = map (MD5.finalize . MD5.update prefixCtx . pack . show) [0..]
+allHashes :: String -> [NS.NibbleString]
+allHashes prefix = map (NS.EvenNibbleString . hash) [0..]
   where
     prefixCtx = MD5.update MD5.init $ pack prefix
+    hash = MD5.finalize . MD5.update prefixCtx . pack . show
+
+interestingPrefix :: NS.NibbleString
+interestingPrefix = NS.pack $ replicate 5 0
+
+interestingHash :: NS.NibbleString -> Bool
+interestingHash = NS.isPrefixOf interestingPrefix
 
 doorCode :: String -> String
 doorCode = take 8 .
            map (head . (`showHex` "") . NS.head . NS.drop 5) .
-           filter (NS.isPrefixOf expectedPrefix) .
-           map NS.EvenNibbleString .
+           filter interestingHash .
            allHashes
-  where
-    expectedPrefix = NS.pack $ replicate 5 0
 
 solve :: String -> IO ()
 solve input = do
