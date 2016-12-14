7681066b9aec397a28b1b75fa7d82de8b677579b
Chris LaRose <cjlarose@gmail.com>
2016-12-13 22:54:32 -0800

    Rewrite parsers in Day 3 using applicative and functor instances

--- src/SquaresWithThreeSides.hs
+++ src/SquaresWithThreeSides.hs
@@ -3,28 +3,12 @@ module SquaresWithThreeSides (solve) where
 import Text.Parsec (parse)
 import Text.Parsec.Char (digit, char)
 import Text.Parsec.Prim (skipMany)
-import Text.Parsec.Combinator (many1, eof, skipMany1)
+import Text.Parsec.Combinator (many1, eof, skipMany1, count)
 
 eol = char '\n'
-
-sideLength = do
-  res <- many1 digit
-  return (read res :: Int)
-
-tripletLine = do
-  skipMany (char ' ')
-  a <- sideLength
-  skipMany1 (char ' ')
-  b <- sideLength
-  skipMany1 (char ' ')
-  c <- sideLength
-  eol
-  return (a, b, c)
-
-triplets = do
-  res <- many1 tripletLine
-  eof
-  return res
+sideLength = read <$> many1 digit
+tripletLine = (\(a:b:c:_) -> (a, b, c)) <$> count 3 (skipMany (char ' ') *> sideLength) <* eol
+triplets = many1 tripletLine <* eof
 
 isTriangle :: (Int, Int, Int) -> Bool
 isTriangle (a, b, c) = a + b > c && a + c > b && b + c > a
