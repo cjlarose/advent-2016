d2e04534bfbd9383b1cd2708142e98e706450f6d
Chris LaRose <cjlarose@gmail.com>
2016-12-13 23:22:24 -0800

    Add ABBA parser for Day 7

--- src/InternetProtocolVersion7.hs
+++ src/InternetProtocolVersion7.hs
@@ -1,5 +1,16 @@
 module InternetProtocolVersion7 (solve) where
 
+import Data.Char (isLower)
+import Text.Parsec.Char (char, lower, satisfy)
+import Text.Parsec.Prim (parse)
+
+abba = do
+  a <- lower
+  b <- satisfy (\x -> isLower x && x /= a)
+  char b
+  char a
+
 solve :: String -> IO ()
 solve input = do
-  putStrLn input
+  let parsed = parse abba "" input
+  print parsed
