f194ed8f8d6b71aabf155f125708935094fded23
Chris LaRose <cjlarose@gmail.com>
2016-12-14 17:14:37 -0800

    Replace non-hypernet-sequence => supernet sequence

--- src/InternetProtocolVersion7.hs
+++ src/InternetProtocolVersion7.hs
@@ -7,7 +7,7 @@ import Text.Parsec.Prim (ParsecT, Stream, parse, many, try, (<|>))
 import Text.Parsec.Char (char, lower, satisfy)
 import Text.Parsec.Combinator (between, many1, eof)
 
-data Sequence = NonHypernetSequence [String] | HypernetSequence [String] deriving Show
+data Sequence = SupernetSequence [String] | HypernetSequence [String] deriving Show
 type IPV7 = [Sequence]
 
 eol :: Stream s m Char => ParsecT s u m Char
@@ -24,8 +24,8 @@ abba = do
 sequenceContents :: Stream s m Char => ParsecT s u m [String]
 sequenceContents = many1 (try abba <|> (pure <$> lower))
 
-nonHypernetSequence :: Stream s m Char => ParsecT s u m Sequence
-nonHypernetSequence = NonHypernetSequence <$> sequenceContents
+supernetSequence :: Stream s m Char => ParsecT s u m Sequence
+supernetSequence = SupernetSequence <$> sequenceContents
 
 hypernetSequence :: Stream s m Char => ParsecT s u m Sequence
 hypernetSequence = HypernetSequence <$> between (char '[') (char ']') sequenceContents
@@ -33,14 +33,14 @@ hypernetSequence = HypernetSequence <$> between (char '[') (char ']') sequenceCo
 none f = not . any f
 
 supportsSnooping :: IPV7 -> Bool
-supportsSnooping xs = any hasAbba nonHypernetSequences && none hasAbba hypernetSequences
+supportsSnooping xs = any hasAbba supernetSequences && none hasAbba hypernetSequences
   where
     hasAbba = any (\x -> length x > 1)
-    nonHypernetSequences = [x | NonHypernetSequence x <- xs]
+    supernetSequences = [x | SupernetSequence x <- xs]
     hypernetSequences = [x | HypernetSequence x <- xs]
 
 ipV7 :: Stream s m Char => ParsecT s u m IPV7
-ipV7 = many (nonHypernetSequence <|> hypernetSequence) <* eol
+ipV7 = many (supernetSequence <|> hypernetSequence) <* eol
 
 ipList :: Stream s m Char => ParsecT s u m [IPV7]
 ipList = many ipV7 <* eof
