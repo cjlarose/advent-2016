267815c5b1141ec05c705650b296a91b8bcc7f43
Chris LaRose <cjlarose@gmail.com>
2016-12-15 09:34:11 -0800

    Move TLS calculation out of main parser

--- src/InternetProtocolVersion7.hs
+++ src/InternetProtocolVersion7.hs
@@ -7,11 +7,23 @@ import Text.Parsec.Prim (ParsecT, Stream, parse, many, try, (<|>))
 import Text.Parsec.Char (char, lower, satisfy)
 import Text.Parsec.Combinator (between, many1, eof)
 
-data Sequence = SupernetSequence [String] | HypernetSequence [String] deriving Show
+data Sequence = SupernetSequence String | HypernetSequence String deriving Show
 type IPV7 = [Sequence]
 
 eol :: Stream s m Char => ParsecT s u m Char
 eol = char '\n'
+
+supernetSequence :: Stream s m Char => ParsecT s u m Sequence
+supernetSequence = SupernetSequence <$> many1 lower
+
+hypernetSequence :: Stream s m Char => ParsecT s u m Sequence
+hypernetSequence = HypernetSequence <$> between (char '[') (char ']') (many1 lower)
+
+ipV7 :: Stream s m Char => ParsecT s u m IPV7
+ipV7 = many (supernetSequence <|> hypernetSequence) <* eol
+
+ipList :: Stream s m Char => ParsecT s u m [IPV7]
+ipList = many ipV7 <* eof
 
 abba :: Stream s m Char => ParsecT s u m String
 abba = do
@@ -21,28 +33,19 @@ abba = do
   char a
   return [a, b, b, a]
 
-sequenceContents :: Stream s m Char => ParsecT s u m [String]
-sequenceContents = many1 (try abba <|> (pure <$> lower))
-
-supernetSequence :: Stream s m Char => ParsecT s u m Sequence
-supernetSequence = SupernetSequence <$> sequenceContents
-
-hypernetSequence :: Stream s m Char => ParsecT s u m Sequence
-hypernetSequence = HypernetSequence <$> between (char '[') (char ']') sequenceContents
+hasAbba :: String -> Bool
+hasAbba s = do
+  let parsed = parse (many1 ((const True <$> try abba) <|> (const False <$> lower))) "" s
+  case parsed of
+    Left _ -> False
+    Right xs -> or xs
 
 none :: (a -> Bool) -> [a] -> Bool
 none f = not . any f
 
-ipV7 :: Stream s m Char => ParsecT s u m IPV7
-ipV7 = many (supernetSequence <|> hypernetSequence) <* eol
-
-ipList :: Stream s m Char => ParsecT s u m [IPV7]
-ipList = many ipV7 <* eof
-
 supportsSnooping :: IPV7 -> Bool
 supportsSnooping xs = any hasAbba supernetSequences && none hasAbba hypernetSequences
   where
-    hasAbba = any (\x -> length x > 1)
     supernetSequences = [x | SupernetSequence x <- xs]
     hypernetSequences = [x | HypernetSequence x <- xs]
 
