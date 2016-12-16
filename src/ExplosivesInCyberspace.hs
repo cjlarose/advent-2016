ab1f8738576c254501e75354cdeffb8b4f17b942
Chris LaRose <cjlarose@gmail.com>
2016-12-16 14:31:03 -0800

    Represent compressed document as sequence of possibly compressed blocks

--- src/ExplosivesInCyberspace.hs
+++ src/ExplosivesInCyberspace.hs
@@ -3,10 +3,11 @@
 module ExplosivesInCyberspace (solve) where
 
 import Text.Parsec.Prim (Stream, ParsecT, parse, many)
-import Text.Parsec.Char (anyChar, digit, char)
+import Text.Parsec.Char (anyChar, digit, char, endOfLine)
 import Text.Parsec.Combinator (count, many1, eof)
 
-type CompressedSequence = ((Int, Int), String)
+data Block = Literal String | Compressed Int Int Block
+type Document = [Block]
 
 intLiteral :: Stream s m Char => ParsecT s u m Int
 intLiteral = read <$> many1 digit
@@ -14,17 +15,18 @@ intLiteral = read <$> many1 digit
 marker :: Stream s m Char => ParsecT s u m (Int, Int)
 marker = (\a b -> (a, b)) <$> (char '(' *> intLiteral <* char 'x') <*> (intLiteral <* char ')')
 
-compressedSequence :: Stream s m Char => ParsecT s u m CompressedSequence
+compressedSequence :: Stream s m Char => ParsecT s u m Block
 compressedSequence = do
   (length, n) <- marker
   text <- count length anyChar
-  return ((length, n), text)
+  return (Compressed length n (Literal text))
 
-compressedData :: Stream s m Char => ParsecT s u m [CompressedSequence]
-compressedData = many compressedSequence <* char '\n' <* eof
+compressedData :: Stream s m Char => ParsecT s u m Document
+compressedData = many compressedSequence <* endOfLine <* eof
 
-decompressedLength :: [CompressedSequence] -> Int
-decompressedLength = sum . map ((\(l, n) -> l * n) . fst)
+decompressedLength :: Block -> Int
+decompressedLength (Literal s) = length s
+decompressedLength (Compressed _ n child) = n * decompressedLength child
 
 solve :: String -> IO ()
 solve input = do
@@ -32,5 +34,5 @@ solve input = do
   case parsed of
     Left err -> print err
     Right xs -> do
-      let len = decompressedLength xs
+      let len = sum . map decompressedLength $ xs
       print len
