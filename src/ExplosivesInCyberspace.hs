3760f0fd6ff36c0596006fa086d25b437f72f209
Chris LaRose <cjlarose@gmail.com>
2016-12-16 15:14:49 -0800

    Parameterize block parser

--- src/ExplosivesInCyberspace.hs
+++ src/ExplosivesInCyberspace.hs
@@ -6,7 +6,7 @@ import Text.Parsec.Prim (Stream, ParsecT, parse, many)
 import Text.Parsec.Char (anyChar, digit, char, endOfLine)
 import Text.Parsec.Combinator (count, many1, eof)
 
-data Block = Literal String | Compressed Int Int Block
+data Block = Literal String | Compressed Int Int Block deriving Show
 type Document = [Block]
 
 intLiteral :: Stream s m Char => ParsecT s u m Int
@@ -15,14 +15,14 @@ intLiteral = read <$> many1 digit
 marker :: Stream s m Char => ParsecT s u m (Int, Int)
 marker = (\a b -> (a, b)) <$> (char '(' *> intLiteral <* char 'x') <*> (intLiteral <* char ')')
 
-compressedSequence :: Stream s m Char => ParsecT s u m Block
-compressedSequence = do
+version1Block :: Stream s m Char => ParsecT s u m Block
+version1Block = do
   (length, n) <- marker
   text <- count length anyChar
   return (Compressed length n (Literal text))
 
-compressedData :: Stream s m Char => ParsecT s u m Document
-compressedData = many compressedSequence <* endOfLine <* eof
+compressedData :: Stream s m Char => ParsecT s u m Block -> ParsecT s u m Document
+compressedData blockParser = many blockParser <* endOfLine <* eof
 
 decompressedLength :: Block -> Int
 decompressedLength (Literal s) = length s
@@ -30,7 +30,7 @@ decompressedLength (Compressed _ n child) = n * decompressedLength child
 
 solve :: String -> IO ()
 solve input = do
-  let parsed = parse compressedData "" input
+  let parsed = parse (compressedData version1Block) "" input
   case parsed of
     Left err -> print err
     Right xs -> do
