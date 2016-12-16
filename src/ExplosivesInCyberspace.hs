9125c2262f11f6ee76aa661e2f3fda8b81ebbb76
Chris LaRose <cjlarose@gmail.com>
2016-12-16 15:23:08 -0800

    Restructure document format to expose self-similarity

--- src/ExplosivesInCyberspace.hs
+++ src/ExplosivesInCyberspace.hs
@@ -6,8 +6,7 @@ import Text.Parsec.Prim (Stream, ParsecT, parse, many)
 import Text.Parsec.Char (anyChar, digit, char, endOfLine)
 import Text.Parsec.Combinator (count, many1, eof)
 
-data Block = Literal String | Compressed Int Int Block deriving Show
-type Document = [Block]
+data Block = Literal String | Compressed Int [Block] deriving Show
 
 intLiteral :: Stream s m Char => ParsecT s u m Int
 intLiteral = read <$> many1 digit
@@ -19,20 +18,20 @@ version1Block :: Stream s m Char => ParsecT s u m Block
 version1Block = do
   (length, n) <- marker
   text <- count length anyChar
-  return (Compressed length n (Literal text))
+  return (Compressed n [Literal text])
 
-compressedData :: Stream s m Char => ParsecT s u m Block -> ParsecT s u m Document
-compressedData blockParser = many blockParser <* endOfLine <* eof
+compressedData :: Stream s m Char => ParsecT s u m Block -> ParsecT s u m Block
+compressedData blockParser = Compressed 1 <$> many blockParser <* endOfLine <* eof
 
 decompressedLength :: Block -> Int
 decompressedLength (Literal s) = length s
-decompressedLength (Compressed _ n child) = n * decompressedLength child
+decompressedLength (Compressed n children) = n * (sum . map decompressedLength $ children)
 
 solve :: String -> IO ()
 solve input = do
   let parsed = parse (compressedData version1Block) "" input
   case parsed of
     Left err -> print err
-    Right xs -> do
-      let len = sum . map decompressedLength $ xs
+    Right doc -> do
+      let len = decompressedLength doc
       print len
