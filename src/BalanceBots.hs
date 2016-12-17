fbb10e24e27373e6f97b6bb22fdb0a866dcc3552
Chris LaRose <cjlarose@gmail.com>
2016-12-16 19:52:23 -0800

    Add parser for Day 10

--- src/BalanceBots.hs
+++ src/BalanceBots.hs
@@ -1,5 +1,43 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module BalanceBots (solve) where
+
+import Text.Parsec.Prim (Stream, ParsecT, parse, (<|>))
+import Text.Parsec.Char (digit, string, endOfLine)
+import Text.Parsec.Combinator (many1, endBy, eof)
+
+newtype Bot = Bot Int deriving Show
+data Destination = ToOutput Int | ToBot Bot deriving Show
+data Stmt = ValueStmt Bot Int | RuleStmt Bot (Destination, Destination) deriving Show
+
+intLiteral :: Stream s m Char => ParsecT s u m Int
+intLiteral = read <$> many1 digit
+
+bot :: Stream s m Char => ParsecT s u m Bot
+bot = string "bot " *> (Bot <$> intLiteral)
+
+valueStmt :: Stream s m Char => ParsecT s u m Stmt
+valueStmt = flip ValueStmt <$>
+            (string "value " *> intLiteral) <*>
+            (string " goes to " *> bot)
+
+valueDestination :: Stream s m Char => ParsecT s u m Destination
+valueDestination = (ToBot <$> bot) <|> (ToOutput <$> output)
+  where
+    output = string "output " *> intLiteral
+
+ruleStmt :: Stream s m Char => ParsecT s u m Stmt
+ruleStmt = (\bot lo hi -> RuleStmt bot (lo, hi)) <$>
+           bot <*>
+           (string " gives low to " *> valueDestination) <*>
+           (string " and high to " *> valueDestination)
+
+instructionList :: Stream s m Char => ParsecT s u m [Stmt]
+instructionList = endBy (valueStmt <|> ruleStmt) endOfLine <* eof
 
 solve :: String -> IO ()
 solve input = do
-  putStrLn input
+  let parsed = parse instructionList "" input
+  case parsed of
+    Left err -> print err
+    Right stmts -> print stmts
