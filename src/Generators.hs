5debde804d7cf270f851a0bb56bc272af01cfd04
Chris LaRose <cjlarose@gmail.com>
2016-12-17 15:04:10 -0800

    Complete parser for day 11

--- src/Generators.hs
+++ src/Generators.hs
@@ -4,7 +4,7 @@ module Generators (solve) where
 
 import Text.Parsec.Prim (Stream, ParsecT, parse, (<|>), try)
 import Text.Parsec.Char (char, string, endOfLine, letter)
-import Text.Parsec.Combinator (many1, sepBy, option, optional)
+import Text.Parsec.Combinator (many1, sepBy, option, optional, eof, endBy)
 
 type Element = String
 data ComponentType = Generator | Microchip deriving Show
@@ -17,11 +17,21 @@ component = (\e t -> (e, t)) <$> many1 letter <*> (generator <|> microchip)
     microchip = Microchip <$ string "-compatible microchip"
 
 componentList :: Stream s m Char => ParsecT s u m [Component]
-componentList = (++) <$>
+componentList = ([] <$ string "nothing relevant") <|>
+                (++) <$>
                 (string "a " *> (component `sepBy` try (string ", a "))) <*>
-                (option [] (pure <$> lastItem) <* char '.' <* endOfLine)
+                (option [] (pure <$> lastItem))
   where
     lastItem = optional (char ',') *> string " and a " *> component
 
+facilityDescription :: Stream s m Char => ParsecT s u m [[Component]]
+facilityDescription = floorDescription `endBy` endOfLine <* eof
+  where
+    floorDescription = (string "The " *> many1 letter *> string " floor contains ") *> (componentList <* char '.')
+
 solve :: String -> IO ()
-solve = putStrLn
+solve input = do
+  let parsed = parse facilityDescription "" input
+  case parsed of
+    Left err -> print err
+    Right floors -> print floors
