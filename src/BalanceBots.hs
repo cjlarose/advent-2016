fb489697604d67b7ebb2b537ff472bd64f1803bd
Chris LaRose <cjlarose@gmail.com>
2016-12-16 20:39:34 -0800

    Construct initial factory state

--- src/BalanceBots.hs
+++ src/BalanceBots.hs
@@ -2,13 +2,17 @@
 
 module BalanceBots (solve) where
 
+import qualified Data.Map.Strict as Map
 import Text.Parsec.Prim (Stream, ParsecT, parse, (<|>))
 import Text.Parsec.Char (digit, string, endOfLine)
 import Text.Parsec.Combinator (many1, endBy, eof)
 
-newtype Bot = Bot Int deriving Show
+newtype Bot = Bot Int deriving (Show, Ord, Eq)
 data Destination = ToOutput Int | ToBot Bot deriving Show
 data Stmt = ValueStmt Bot Int | RuleStmt Bot (Destination, Destination) deriving Show
+type Rules = Map.Map Bot (Destination, Destination)
+type BotPossession = Map.Map Bot [Int]
+type FactoryState = (Rules, BotPossession)
 
 intLiteral :: Stream s m Char => ParsecT s u m Int
 intLiteral = read <$> many1 digit
@@ -34,6 +38,12 @@ ruleStmt = (\bot lo hi -> RuleStmt bot (lo, hi)) <$>
 
 instructionList :: Stream s m Char => ParsecT s u m [Stmt]
 instructionList = endBy (valueStmt <|> ruleStmt) endOfLine <* eof
+
+initialState :: [Stmt] -> FactoryState
+initialState stmts = (rules, possessions)
+  where
+    rules = Map.fromList [(b, ds) | (RuleStmt b ds) <- stmts]
+    possessions = Map.fromListWith (++) [(b, [v]) | (ValueStmt b v) <- stmts]
 
 solve :: String -> IO ()
 solve input = do
