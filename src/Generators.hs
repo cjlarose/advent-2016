b914fede5145927dc3fca50f24b7a340df8479c3
Chris LaRose <cjlarose@gmail.com>
2016-12-18 21:17:36 -0800

    Compute valid next steps given a facility state...
    ...with the nondeterminism monad!!

--- src/Generators.hs
+++ src/Generators.hs
@@ -3,6 +3,7 @@
 module Generators (solve) where
 
 import qualified Data.Set as Set
+import Data.List (subsequences)
 import Text.Parsec.Prim (Stream, ParsecT, parse, (<|>), try)
 import Text.Parsec.Char (char, string, endOfLine, letter)
 import Text.Parsec.Combinator (many1, sepBy, option, optional, eof, endBy)
@@ -12,6 +13,7 @@ data ComponentType = Generator | Microchip deriving (Show, Ord, Eq)
 type Component = (Element, ComponentType)
 type Floor = Set.Set Component
 type FacilityZipper = ([Floor], Floor, [Floor]) -- floors above, current floor, floors below
+data ElevatorOption = Up | Down deriving Show
 
 component :: Stream s m Char => ParsecT s u m Component
 component = (\e t -> (e, t)) <$> many1 letter <*> (generator <|> microchip)
@@ -46,6 +48,30 @@ moveDown (above, cursor, x:below) cs = (newUpper:above, newCurrent, below)
   where
     newUpper = Set.difference cursor cs
     newCurrent = Set.union x cs
+
+move :: FacilityZipper -> ElevatorOption -> Set.Set Component -> FacilityZipper
+move facility Up = moveUp facility
+move facility Down = moveDown facility
+
+safeFloor :: Floor -> Bool
+safeFloor floor = not generatorOnFloor || null unshieldedChips
+  where
+    generatorElements = Set.fromList [e | (e, Generator) <- Set.toList floor]
+    unshieldedChips = [e | (e, Microchip) <- Set.toList floor, e `Set.notMember` generatorElements]
+    generatorOnFloor = not . null $ generatorElements
+
+elevatorOptions :: FacilityZipper -> [ElevatorOption]
+elevatorOptions (floorsAbove, _, floorsBelow) = concat $ zipWith (\a b -> if null b then [] else [a]) [Up, Down] [floorsAbove, floorsBelow]
+
+potentialPayloads :: Floor -> [Set.Set Component]
+potentialPayloads = map Set.fromList . filter (\x -> not (null x) && length x < 3) . subsequences . Set.toList
+
+validNextSteps :: FacilityZipper -> [FacilityZipper]
+validNextSteps facility@(floorsAbove, floor, floorsBelow) = do
+  payload <- potentialPayloads floor
+  direction <- elevatorOptions facility
+  let newFacility@(_, newFloor, _) = move facility direction payload
+  if safeFloor newFloor then return newFacility else []
 
 solve :: String -> IO ()
 solve input = do
