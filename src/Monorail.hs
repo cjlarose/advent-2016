7e3129ef099fe49466f1b6374047c400928a5bb9
Chris LaRose <cjlarose@gmail.com>
2016-12-20 23:51:08 -0800

    Add fn for running machine

--- src/Monorail.hs
+++ src/Monorail.hs
@@ -4,7 +4,8 @@ module Monorail (solve) where
 
 import qualified Data.Map.Strict as Map
 import Control.Monad (unless)
-import Control.Monad.State (State, get, modify)
+import Control.Monad.State (State, get, modify, runState)
+import Control.Monad.Loops (untilM_)
 import Text.Parsec.Prim (Stream, ParsecT, many, (<|>), parse)
 import Text.Parsec.Combinator (eof, many1, endBy, option)
 import Text.Parsec.Char (digit, endOfLine, string, oneOf, char)
@@ -12,6 +13,8 @@ import Text.Parsec.Char (digit, endOfLine, string, oneOf, char)
 newtype Register = Register Char deriving (Show, Ord, Eq)
 data Machine = Machine { programCounter :: Int, registers :: Map.Map Register Int }
 type Instruction = State Machine ()
+
+machineStart = Machine { programCounter = 0, registers = Map.empty }
 
 readRegister :: Register -> State Machine Int
 readRegister r = do
@@ -23,6 +26,12 @@ updateRegister f r = do
   currentVal <- readRegister r
   let newVal = f currentVal
   modify (\m -> m { registers = Map.insert r newVal (registers m) })
+
+getProgramCounter :: State Machine Int
+getProgramCounter = get >>= return . programCounter
+
+advanceProgramCounter :: State Machine ()
+advanceProgramCounter = jump 1
 
 jump :: Int -> State Machine ()
 jump relJump = modify (\m -> m { programCounter = programCounter m + relJump })
@@ -37,19 +46,19 @@ register :: Stream s m Char => ParsecT s u m Register
 register = Register <$> oneOf "abcd"
 
 copyImmediate :: Stream s m Char => ParsecT s u m Instruction
-copyImmediate = (\x r -> updateRegister (const x) r) <$> immediate <*> (char ' ' *> register)
+copyImmediate = (\x r -> updateRegister (const x) r >> advanceProgramCounter) <$> immediate <*> (char ' ' *> register)
 
 copyRegister :: Stream s m Char => ParsecT s u m Instruction
-copyRegister = (\src dst -> readRegister src >>= (\x -> updateRegister (const x) dst)) <$> register <*> (char ' ' *> register)
+copyRegister = (\src dst -> readRegister src >>= (\x -> updateRegister (const x) dst) >> advanceProgramCounter) <$> register <*> (char ' ' *> register)
 
 copyInstruction :: Stream s m Char => ParsecT s u m Instruction
 copyInstruction = string "cpy " *> (copyRegister <|> copyImmediate)
 
 incInstruction :: Stream s m Char => ParsecT s u m Instruction
-incInstruction = updateRegister (+ 1) <$> (string "inc " *> register)
+incInstruction = (\r -> updateRegister (+ 1) r >> advanceProgramCounter) <$> (string "inc " *> register)
 
 decInstruction :: Stream s m Char => ParsecT s u m Instruction
-decInstruction = updateRegister (subtract 1) <$> (string "dec " *> register)
+decInstruction = (\r -> updateRegister (subtract 1) r >> advanceProgramCounter) <$> (string "dec " *> register)
 
 unconditionalJump :: Stream s m Char => ParsecT s u m Instruction
 unconditionalJump = jump <$> (immediate *> char ' ' *> immediate)
@@ -66,10 +75,20 @@ instruction = copyInstruction <|> incInstruction <|> decInstruction <|> jumpInst
 instructionList :: Stream s m Char => ParsecT s u m [Instruction]
 instructionList = instruction `endBy` endOfLine <* eof
 
+runMachineUntilHalt :: [Instruction] -> State Machine (Map.Map Register Int)
+runMachineUntilHalt xs = do
+  let step = getProgramCounter >>= (\pc -> xs !! pc)
+  let halted = getProgramCounter >>= (\pc -> return (pc >= length xs))
+
+  step `untilM_` halted
+  m <- get
+  return (registers m)
+
 solve :: String -> IO ()
 solve input = do
   let parsed = parse instructionList "" input
   case parsed of
     Left err -> print err
     Right instructions -> do
-      print . length $ instructions
+      let (registerState, _) = runState (runMachineUntilHalt instructions) machineStart
+      print registerState
