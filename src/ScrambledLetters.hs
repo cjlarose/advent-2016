09db02e4a4fe8ff254410153f62e173b73282793
Chris LaRose <cjlarose@gmail.com>
2016-12-30 22:56:09 -0700

    Complete Day 21, Part 1

--- src/ScrambledLetters.hs
+++ src/ScrambledLetters.hs
@@ -2,6 +2,8 @@
 
 module ScrambledLetters (solve) where
 
+import Data.List (elemIndex)
+import Data.Maybe (fromJust)
 import Text.Parsec.Prim (Stream, ParsecT, parse, (<|>), try)
 import Text.Parsec.Char (endOfLine, string, char, digit, letter)
 import Text.Parsec.Combinator (endBy, eof, many1)
@@ -10,26 +12,46 @@ type Instruction = String -> String
 data Rotation = L | R
 
 -------------------------------------------------------------------------------
+-- Utils
+-------------------------------------------------------------------------------
+
+splitAt2 :: Int -> Int -> String -> (String, String, String)
+splitAt2 i j str = (prefix, middle, suffix)
+  where
+    (prefix, rest) = splitAt i str
+    (middle, suffix) = splitAt (j - i) rest
+
+-------------------------------------------------------------------------------
 -- Instructions
 -------------------------------------------------------------------------------
 
 rotateString :: Rotation -> Int -> String -> String
-rotateString _ _ = id
+rotateString L n str = take (length str) . drop (n `mod` length str) $ str ++ str
+rotateString R n str = take (length str) . drop (length str - (n `mod` length str)) $ str ++ str
 
 rotateBasedOnPositionOfChar :: Char -> String -> String
-rotateBasedOnPositionOfChar _ = id
+rotateBasedOnPositionOfChar x str = rotateString R n str
+  where
+    i = fromJust $ elemIndex x str
+    n = 1 + i + (if i >= 4 then 1 else 0)
 
 swapPositions :: Int -> Int -> String -> String
-swapPositions i j = id
+swapPositions i j str | i > j = swapPositions j i str
+                      | otherwise = prefix ++ [a] ++ middle ++ [b] ++ suffix
+  where
+    (prefix, b:middle, a:suffix) = splitAt2 i j str
 
 swapChars :: Char -> Char -> String -> String
-swapChars _ _ = id
+swapChars a b str = swapPositions (fromJust $ elemIndex a str) (fromJust $ elemIndex b str) str
 
 reverseSubstring :: Int -> Int -> String -> String
-reverseSubstring i j = id
+reverseSubstring i j str = prefix ++ reverse middle ++ suffix
+  where
+    (prefix, middle, suffix) = splitAt2 i (j + 1) str
 
 move :: Int -> Int -> String -> String
-move i j = id
+move i j str | i > j = let (prefix, middle, c:suffix) = splitAt2 j i str in prefix ++ [c] ++ middle ++ suffix
+             | otherwise = let (prefix, c:middle, suffix) = splitAt2 i (j + 1) str in prefix ++ middle ++ [c] ++ suffix
 
 -------------------------------------------------------------------------------
 -- Parsers
